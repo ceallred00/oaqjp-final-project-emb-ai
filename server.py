2495ef7d428c1bbb0397f9988632e6c9cbba4af6
ceallred00 <cea33@students.uwf.edu>
2025-04-21 11:30:50 -0400

    Committing through Task 6

---
+++ server.py
@@ -0,0 +1,30 @@
+# Import Flask, render_template, request from the flask pramework package :
+from flask import Flask, render_template, request
+# Import the sentiment_analyzer function from the package created:
+from EmotionDetection.emotion_detection import emotion_detector
+
+# Initiate the Flask app:
+app = Flask("Emotion Detector")
+
+@app.route('/emotionDetector')
+def emot_detector():
+    text_to_analyzer = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyzer)
+
+    res = (f'For the given statement, the system response is "anger": {response["anger"]}, '
+    f'"disgust" : {response["disgust"]}, "fear": {response["fear"]}, '
+    f'"joy": {response["joy"]} and "sadness": {response["sadness"]}. '
+    f'The dominant emotion is {response["dominant_emotion"]}.')
+
+    return res
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+# This functions executes the flask app and deploys it on localhost:5000
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
